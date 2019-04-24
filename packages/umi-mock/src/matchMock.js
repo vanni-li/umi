c06e462b4c945632999124c738b3d82d4fc20269
libing <icoon@qq.com>
2019-04-24 19:35:29 +0800

    add note

--- packages/umi-mock/src/matchMock.js
+++ packages/umi-mock/src/matchMock.js
@@ -20,9 +20,11 @@ export default function(req, mockData) {
 
   for (const mock of mockData) {
     const { method, re, keys } = mock;
+    // NOTE: 如果 mathod 和 路由正则都能匹配，则返回此 mock 数据
     if (method === targetMethod) {
       const match = re.exec(targetPath);
       if (match) {
+        // NOTE: 比如，解析地址里的 `/user/:name`，req.params 里存入 name 值： req.params['name'] = xxx
         const params = {};
         for (let i = 1; i < match.length; i += 1) {
           const key = keys[i - 1];
